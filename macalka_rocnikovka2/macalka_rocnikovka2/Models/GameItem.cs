8c38fcb7cb03eccc6b65645cf58bb1d3aab61309
Sebastian Macálka <61810macalka@student.sstebrno.cz>
2026-04-20 14:27:14 +0200

    Add files via upload

---
+++ macalka_rocnikovka2/macalka_rocnikovka2/Models/GameItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace macalka_rocnikovka2.Models
+{
+    class GameItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public bool IsInInventory { get; set; }
+
+
+        public GameItem()
+        {
+            IsInInventory = false;
+
+        }
+
+        public GameItem(string id, string name, string description, string imagePath)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            ImagePath = imagePath;
+            IsInInventory = false;
+        }
+    }
+}
---
+++ macalka_rocnikovka2/macalka_rocnikovka2/Models/Scene.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace macalka_rocnikovka2.Models
+{
+    class Scene
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string BackgroundImagePath { get; set; }
+        public List<GameItem> ItemsInScene { get; set; }
+        public List<Character> CharactersInScene { get; set; }
+        public int? NextSceneId { get; set; }
+
+
+        public Scene()
+        {
+            ItemsInScene = new List<GameItem>();
+            CharactersInScene = new List<Character>();
+            NextSceneId = null;
+        }
+
+        public Scene(int id, string name, string description, string backgroundImagePath)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            BackgroundImagePath = backgroundImagePath;
+            ItemsInScene = new List<GameItem>();
+            CharactersInScene = new List<Character>();
+            NextSceneId = null;
+        }
+
+        public void AddItem(GameItem item)
+        {
+            if (item != null)
+            {
+                ItemsInScene.Add(item);
+            }
+        }
+        public void RemoveItem(GameItem item)
+        {
+            if (ItemsInScene.Contains(item))
+            {
+                ItemsInScene.Remove(item);
+            }
+        }
+
+        public void AddCharacter(Character character)
+            {
+                if (character != null)
+                {
+                    CharactersInScene.Add(character);
+                }
+        }
+
+        public int GetItemCount()
+        {
+            return ItemsInScene.Count;
+        }   
+    }
+}
